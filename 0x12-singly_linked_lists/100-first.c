91ff8a598c3cdd143c9dbeebe9d8686bd0020f36
Aguert55 <oussamaaguert5@gmail.com>
2023-10-19 16:34:07 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-void pre_main_message(void);
+
+void first(void) __attribute__ ((constructor));
+
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * first - prints a sentence before the main
+ * function is executed
  */
-int main(void)
+void first(void)
 {
-	pre_main_message();
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
